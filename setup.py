390b8782ac42e0b1830eea4494268eab0cf7d72e
Gouthum Kharvi <gouthumkharvi9845@gmail.com>
2026-06-18 18:03:48 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+name="ecosortvision",
+version="0.0.1",
+author="Gouthum Kharvi",
+author_email="gouthumkharvi1899@gmail.com",
+packages=find_packages(),
+install_requires=[]
+)
